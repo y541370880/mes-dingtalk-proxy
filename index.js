d6470965b91477ec7759b8176c3e19541ca7d1f8
Mingxin <66431093+y541370880@users.noreply.github.com>
2026-07-03 14:06:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 let messageQueue = [];
 
 app.post("/api/receive", (req, res) => {
